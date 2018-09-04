34e8a8ff1ee59731d11a65af689f5ba5d66793a1
troy <troywhitetw@ymail.com>
2018-09-04 10:02:31 +0800

    20180829 homework

---
+++ 20180829_homework/test0829_GoAhead/test0829_GoAhead/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test0829_GoAhead {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        void SendMail(int i) {
+            try {
+                if (i == 4) {
+                    // Email address format is wrong...
+                    Exception ex = new Exception("Email format error");
+                    throw ex;
+                }
+
+                listBox1.Items.Add(i);
+            } catch (Exception e) {
+                listBox1.Items.Add(e.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            for (int i = 1; i <= 9; i++) {
+                SendMail(i);
+            }
+        }
+    }
+}
